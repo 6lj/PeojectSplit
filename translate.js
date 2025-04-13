87df0936c5d36287550203892a7827dbe65f3075
Mohmmad hissyn Aon (ENDUP) <hhfw809o@gmail.com>
2025-04-13 17:29:16 +0300

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,11 +1,19 @@
 
-const words = [
-    'مرحبا',   
-    'وداعا',   
-    'شكرا',   
-    'من فضلك', 
-    'نعم',     
-    'لا',     
-    'آسف',    
-    'مساعدة'   
-];
+const translations = {
+    'انشاء معرف': 'Generate ID',
+    'مرحبا': 'Hello',
+    'وداعا': 'Goodbye',
+    'شكرا': 'Thank you',
+    'من فضلك': 'Please',
+    'نعم': 'Yes',
+    'لا': 'No',
+    'آسف': 'Sorry',
+    'مساعدة': 'Help',
+};
+
+function translate(word) {
+    return translations[word] || word; 
+}
+
+
+window.translate = translate;
